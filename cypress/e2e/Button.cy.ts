b5337c21828683a7fb3ddd9321740369247f47bb
Kamolov Daler <kamolovd@HLB-ADM85MAC.local>
2025-07-11 17:34:24 +0500

    change: redirect in cy

--- cypress/e2e/Button.cy.ts
+++ cypress/e2e/Button.cy.ts
@@ -1,6 +1,8 @@
+/// <reference types="cypress" />
+
 describe('CounterButton', () => {
     it('increments count when clicked', () => {
-        cy.visit('/');
+        cy.visit('/frontOps-practice/');
         cy.contains('Clicked 0 times').should('exist');
         cy.contains('Clicked 0 times').click();
         cy.contains('Clicked 1 times').should('exist');
