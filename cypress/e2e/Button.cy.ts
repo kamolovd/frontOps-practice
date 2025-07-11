3d7c323fcaf462be37d862c37b218a13a0946cb9
Kamolov Daler <kamolovd@HLB-ADM85MAC.local>
2025-07-11 18:00:16 +0500

    fix: url path

--- cypress/e2e/Button.cy.ts
+++ cypress/e2e/Button.cy.ts
@@ -2,7 +2,7 @@
 
 describe('CounterButton', () => {
     it('increments count when clicked', () => {
-        cy.visit('/frontOps-practice/');
+        cy.visit('/');
         cy.contains('Clicked 0 times').should('exist');
         cy.contains('Clicked 0 times').click();
         cy.contains('Clicked 1 times').should('exist');
